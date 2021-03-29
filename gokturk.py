b7e58697d8fd261c39abd3321de27e194cd66860
gokturk-tim <80634786+gokturk-tim@users.noreply.github.com>
2021-03-29 10:25:49 +0300

    Add files via upload

---
+++ gokturk.py
@@ -0,0 +1,29 @@
+﻿import os
+import time
+
+os.system("apt install figlet")
+os.system("clear")
+os.system("figlet GOKTURK TIM")
+print("""
+Bu araçla otomatik olarak IP Adres değiştirebilirsiniz, değeri saniye olarak girin. GökTürk Tim gururla sunar...
+
+~Darhan Bir Türk~ ~BayındırJapheth~
+""")
+
+sure = input("IP Değişim Süre(saniye) : ")
+
+os.system("anonsurf start")
+os.system("clear")
+print("Yeni IP Adres :")
+print("-----------------------------")
+os.system("curl icanhazip.com")
+print("-----------------------------")
+
+while True:
+	time.sleep(sure)
+	os.system("anonsurf restart")
+	os.system("clear")
+	print("Yeni IP Adres :")
+	print("-----------------------------")
+	os.system("curl icanhazip.com")
+	print("-----------------------------")
